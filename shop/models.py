fd17f2cc927df0aa3b9b4cfe034f46a9778a27e9
Learan-Canning <learancanning@mail.com>
2026-04-03 23:02:52 +0100

    Add Product model with admin interface and media configuration

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField(
+        default="This is a synthetic research chemical supplied as lyophilized powder. Third-party tested at 99%+ purity. For laboratory research use only."
+    )
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
