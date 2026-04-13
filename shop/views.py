8c8bce47aa9a1cd7a50bcaeb7b74bce4cd4fc54c
Learan-Canning <learancanning@mail.com>
2026-04-13 19:34:02 +0100

    create certificate page ready for polish

--- shop/views.py
+++ shop/views.py
@@ -29,10 +29,68 @@ def shop(request):
     )
 
 
+# certificates view example
 def certificates(request):
-    return render(request, "certificates.html")
+    certificates = [
+        {
+            "title": "Retatrutide 10mg Certificate of Analysis",
+            "product": "Retatrutide 10mg",
+            "batch": "RET-2026-001",
+            "purity": 99.1,
+            "tested": "Mar 1, 2026",
+        },
+        {
+            "title": "Retatrutide 20mg Certificate of Analysis",
+            "product": "Retatrutide 20mg",
+            "batch": "RET-2026-002",
+            "purity": 98.9,
+            "tested": "Mar 1, 2026",
+        },
+        {
+            "title": "Retatrutide 30mg Certificate of Analysis",
+            "product": "Retatrutide 30mg",
+            "batch": "RET-2026-003",
+            "purity": 99.0,
+            "tested": "Mar 1, 2026",
+        },
+        {
+            "title": "Tirzepatide 10mg Certificate of Analysis",
+            "product": "Tirzepatide 10mg",
+            "batch": "TIR-2026-001",
+            "purity": 98.8,
+            "tested": "Mar 5, 2026",
+        },
+        {
+            "title": "Tirzepatide 20mg Certificate of Analysis",
+            "product": "Tirzepatide 20mg",
+            "batch": "TIR-2026-002",
+            "purity": 99.2,
+            "tested": "Mar 5, 2026",
+        },
+        {
+            "title": "BPC-157 5mg Certificate of Analysis",
+            "product": "BPC-157 5mg",
+            "batch": "BPC-2026-001",
+            "purity": 99.2,
+            "tested": "Feb 15, 2026",
+        },
+        {
+            "title": "TB-500 5mg Certificate of Analysis",
+            "product": "TB-500 5mg",
+            "batch": "TB5-2026-001",
+            "purity": 98.7,
+            "tested": "Feb 20, 2026",
+        },
+        {
+            "title": "GHK-Cu 50mg Certificate of Analysis",
+            "product": "GHK-Cu 50mg",
+            "batch": "GHK-2026-001",
+            "purity": 98.6,
+            "tested": "Feb 28, 2026",
+        },
+    ]
 
-
+    return render(request, "certificates.html", {"certificates": certificates})
 
 
 def about(request):
