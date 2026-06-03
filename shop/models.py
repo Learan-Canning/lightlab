2b8588ae202487fd2c4b422f1615de9ec803b805
Learan-Canning <learancanning@mail.com>
2026-06-03 20:14:07 +0100

    Finalize order/address migrations and checkout flow

--- shop/models.py
+++ shop/models.py
@@ -42,11 +42,11 @@ class Order(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=200, blank=True)
 
-    address_line_1 = models.CharField(max_length=255)
-    address_line_2 = models.CharField(max_length=255, blank=True)
-    town_or_city = models.CharField(max_length=100)
-    county = models.CharField(max_length=100, blank=True)
-    postcode = models.CharField(max_length=20)
+    address_line_1 = models.CharField(max_length=255, blank=True, default="")
+    address_line_2 = models.CharField(max_length=255, blank=True, default="")
+    town_or_city = models.CharField(max_length=100, blank=True, default="")
+    county = models.CharField(max_length=100, blank=True, default="")
+    postcode = models.CharField(max_length=20, blank=True, default="")
     country = models.CharField(max_length=100, default="United Kingdom")
 
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("0.00"))
